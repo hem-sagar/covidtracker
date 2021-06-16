f450a166fe0e8dea748861780a0247f886059599
hem-sagar <hemsagar071@gmail.com>
2021-06-16 07:26:58 +0545

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useEffect,useState} from 'react';
+import ReactDom from 'react-dom';
+import logo from './img/logo.png';
+import './css/index.css';
+const App = () =>{
+    const [data,setData] = useState([]);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    const getCovidData = async() => {
+        try {
+            // const res = await fetch('https://cors-anywhere.herokuapp.com/https://api.covidtracking.com/v1/us/daily.json');
+            const res = await fetch('https://api.covid19api.com/summary');
+            const actualData = await res.json();
+            console.log(actualData.Countries[121]);
+            setData(actualData.Countries[121]);
+            
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        getCovidData();
+    },[]);
+
+
+    return (
+        <div className="hero">     
+            <div className="ui container">
+                <div className="ui centered card">
+                    <div className="image">
+                        <img src={logo} alt="Logo"/>
+                    </div>
+                </div>
+                <h1 className="ui center aligned icon header">
+                    {data.Country}
+                </h1>
+                <h2 className="ui center aligned icon header">
+                {data.Date}
+                </h2>
+                
+                <div className="details">
+                    <div className = "ui centered cards">
+                        <div className="card">
+                            <div className="center aligned content">
+                                <div className="header">
+                                    <h1>Total Confirmed</h1>
+                                </div>
+                            </div>
+                            <div className="extra content">
+                                <div className="center aligned description">
+                                    <h2>{data.TotalConfirmed}</h2>
+                                </div>
+                            </div>                    
+                        </div>
+                        <div className="card">
+                            <div className="center aligned content">
+                                <div className="header">
+                                    <h1>Total Recovered</h1>
+                                </div>
+                            </div>
+                            <div className="extra content">
+                                <div className="center aligned description">
+                                    <h2>{data.TotalRecovered}</h2>
+                                </div>
+                            </div>
+                            
+                        </div>
+
+                        <div className="card">
+                            <div className="center aligned content">
+                                <div className="header">
+                                    <h1>Total Confirmed</h1>
+                                </div>
+                            </div>
+                            <div className="extra content">
+                                <div className="center aligned description">
+                                    <h2>{data.TotalConfirmed}</h2>
+                                </div>
+                            </div>                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ReactDom.render(
+    <App />,
+    document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
